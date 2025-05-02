0a11b19525b45d2ad937027b916c8c5e2a36bbb1
smmahfuzurrahman <smmahfuz787@gmail.com>
2025-05-02 23:09:12 +0600

    veriable, array, object

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-"# road-to-javascript" 
-"# road-to-javascript" 
+Brendan Eich created a programming language called Mocha in 1995 while working at a company called Netscape Communications.
+
--- intro.js
+++ intro.js
@@ -1,1 +1,15 @@
-console.log ("Hello, World!");
+// Print whatever you want. just type what you want to print in the console.
+console.log ("Hello, JavaScript!"); 
+
+// for veriables, use let or const instead of var.
+let name = 'Mahfuzur Rahman';  // string
+let age = 25; // number
+let isStudent = true; // boolean
+let hobbies = ['reading', 'traveling', 'coding']; // array
+let address = { 
+    street: '123 Main St',
+    city: 'Dhaka',
+    country: 'Bangladesh'
+};
+
+
