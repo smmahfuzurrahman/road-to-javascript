8eac1722298ff0881ddc5787514db12c99873d81
smmahfuzurrahman <smmahfuz787@gmail.com>
2025-05-03 23:05:11 +0600

    Array Learning

--- intro.js
+++ intro.js
@@ -1,15 +1,24 @@
 // Print whatever you want. just type what you want to print in the console.
-console.log ("Hello, JavaScript!"); 
+// console.log ("Hello, JavaScript!"); 
 
 // for veriables, use let or const instead of var.
-let name = 'Mahfuzur Rahman';  // string
+const names = 'S.M. Mahfuzur Rahman'; // string
+
+// console.log(names); // string
+
 let age = 25; // number
 let isStudent = true; // boolean
 let hobbies = ['reading', 'traveling', 'coding']; // array
+for (let i = 0; i < hobbies.length; i++) {
+    console.log(hobbies[i]); // array
+}
+hobbies.map((hobby) => console.log(hobby)); // array
 let address = { 
     street: '123 Main St',
     city: 'Dhaka',
     country: 'Bangladesh'
 };
-
+for (let i in address) {
+    console.log(`${i}: ${address[i]}`); // object
+}
 
